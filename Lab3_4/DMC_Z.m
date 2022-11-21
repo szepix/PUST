4c9618665441eb81e31d69c3a79d47105b98d998
Daniel Szepietowski <44882451+szepix@users.noreply.github.com>
2022-11-21 14:51:30 +0100

    Add files via upload

--- Lab3_4/DMC_Z.m
+++ Lab3_4/DMC_Z.m
@@ -4,6 +4,8 @@ initSerialControl COM2% initialise com port
 W1 = 50;
 G1 = 26;
 i = 0;
+
+h = animatedline('Marker','o');
 %% Parametry Regulatora
 
 Tp = 1;
@@ -15,25 +17,25 @@ for i = 1:500
     s(i) = (values(i) - values(1))/20;
 end
 
-% values_z = importdata("skok_z_46.txt");
-s_z = zeros(1,500);
-% for i=1:500
-%     s_z(i) = (values(i) - values(1))/20;
-% end
+values_z = importdata("skok_z_30.txt");
+s_z = zeros(1,400);
+for i=1:400
+    s_z(i) = (values(i) - values(1))/30;
+end
 
 %Horyzonty
 D = 400; %Horyzont Dynamiki
 Dz = 400; %Horyzont Dynamiki toru Z
-N= 20;    %Horyzont predykcji
-Nu = 20; %Horyzont sterowania
+N= 40;    %Horyzont predykcji
+Nu = 40; %Horyzont sterowania
 
 %Współczynnik kary
-lamb = 0.3;
+lamb = 0.35;
 k = D+1;
 %War początkowe
 u(1:k) = 26; y(1:k) = 32;
-yzad(k:500+k) = 35;
-yzad(500+k:1000+k) = 30;
+yzad(k:300+k) = 35;
+yzad(300+k:600+k) = 38;
 e(1:k) = 0;
 
 %Macierz M
@@ -58,23 +60,28 @@ for i=1:N
    end
 end
 
-MZP = zeros(N, Dz);
+MZP = zeros(N, Dz-1);
 for i=1:N
    for j=1:Dz-1
+      if i+j<=Dz
       MZP(i,j)=s_z(i+j)-s_z(j);
+      else
+      MZP(i,j)=s(Dz)-s(j);
+      end
    end
 end
 
 K = ((M'*M + lamb * eye(Nu))^(-1))* M';
 DUp = zeros(D-1, 1);
-dZ = zeros(Dz, 1);
+dZ = zeros(Dz-1, 1);
 Z = zeros(k,1);
 Y = zeros(N,1);
-
+Z_war = 0;
 while(1)
     %% obtaining measurements
     measurements = readMeasurements(1); % read measurements from 1 to 7
     %% processing of the measurements and new control values calculation
+    
     for n=1:N
     %yzad dla horyzontu predykcji
         Y_zad(n,1) = yzad(k);
@@ -82,9 +89,11 @@ while(1)
     %symulacja obiektu
     y(k)=measurements;
     %zakłócenie
-    if(y(k) > yzad(k))
-        Z(k:500+(D+1)) = 30;
+    if(y(k) >= yzad(k))
+        Z_war = 30;
     end
+    Z(k) = Z_war;
+    disp(Z_war);
     %stała trajektoria referencyjna
     for n=1:N
         Y(n) = y(k);
@@ -95,9 +104,12 @@ while(1)
     for n = 1:D-1
         DUp(n) = u(k-n) - u(k-n-1);
     end
-    dz=Z(k)-Z(k-1);
-    Yo = MP*DUp+Y+MZP*dz;
-
+    for n = 1:Dz-1
+        dZ(n) = Z(k-n) - Z(k-n-1); 
+    end
+%     dz=Z(k)-Z(k-1);
+    Yo = MP*DUp+Y+MZP*dZ;
+%     Yo = MP*DUp+Y;
     DU = K*(Y_zad - Yo);
 
     u(k)=u(k-1)+DU(1); 
@@ -113,12 +125,17 @@ while(1)
 
     %% sending new values of control signals
 %     sendControlsToG1AndDisturbance
+%     dlmwrite('DMC_NASTAWY_2_Z30.txt', measurements, '-append');
+%     dlmwrite('DMC_NASTAWY_2_Z30_STEROWANIE.txt', wejscie_u(k), '-append');
     sendControls([ 1, 2, 3, 4, 6], ... send for these elements
                  [W1, 0, 0, 0, 0]);  % new corresponding control values
     sendControlsToG1AndDisturbance(wejscie_u(k), Z(k));
     %%36 26 46 26 16
+    addpoints(h,k-400,measurements);
+    drawnow;
+    disp(measurements);
+    disp(wejscie_u(k));
     k = k + 1;
-    
     %% synchronising with the control process
     waitForNewIteration(); % wait for new batch of measurements to be ready
 end
---
+++ Lab3_4/MinimalWorkingExample.m
@@ -0,0 +1,35 @@
+function MinimalWorkingExample()
+    addpath('D:\SerialCommunication'); % add a path to the functions
+    initSerialControl COM2% initialise com port
+    W1 = 100;
+    G1 = 26;
+    Z = 0;
+    h = animatedline('Marker','o');
+    i = 0;
+    while(1)
+        %% obtaining measurements
+        measurements = readMeasurements(1); % read measurements from 1 to 7
+        
+%         dlmwrite('skok_z_30.txt', measurements, '-append');
+        
+        %% processing of the measurements and new control values calculation
+        disp(measurements); % process measurements
+
+% 1. W1 – wentylator sterowany sygnałem 0-100 (co odpowiada mocy 0%-100% elementu),
+% 2. W2 – wentylator sterowany sygnałem 0-100 (co odpowiada mocy 0%-100% elementu),
+% 3. W3 – wentylator sterowany sygnałem 0-100 (co odpowiada mocy 0%-100% elementu),
+% 4. W4 – wentylator sterowany sygnałem 0-100 (co odpowiada mocy 0%-100% elementu),
+% 5. G1 – grzałka sterowana sygnałem 0-100 (co odpowiada mocy 0%-100% elementu),
+% 6. G2 – grzałka sterowana sygnałem 0-100 (co odpowiada mocy 0%-100% elementu
+        %% sending new values of control signals
+        sendControls([ 1, 2, 3, 4, 6], ... send for these elements
+                     [W1, 0, 0, 0, 0]);  % new corresponding control values
+        sendControlsToG1AndDisturbance(G1,Z);
+        %%10 0 20 0 30
+        addpoints(h,i,measurements);
+        drawnow;
+        i = i + 1;
+        %% synchronising with the control process
+        waitForNewIteration(); % wait for new batch of measurements to be ready
+    end
+end
